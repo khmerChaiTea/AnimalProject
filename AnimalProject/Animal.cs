d1f1780c6214e11f7edc830b1d8c1ca53b4f6d07
pchai <prempreychhay@gmail.com>
2024-07-22 15:46:08 -0700

    Add project files.

---
+++ AnimalProject/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalProject
+{
+    internal class Animal
+    {
+        public string name;
+        public int age;
+
+        public Animal()
+        {
+            name = string.Empty; // ""
+            age = 0;
+        }
+
+
+    }
+}
---
+++ AnimalProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Animal();
+            t1.name = "Test";
+            t1.age = 1;
+            Console.WriteLine($"Name: {t1.name}, Age: {t1.age}");
+        }
+    }
+}
